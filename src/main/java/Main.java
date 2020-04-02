c1ce537ed3cb8d8169219513d230bcecac980f42
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-04-02 13:43:17 +0200

    setUp maven files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
-package main;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    MyClass myClass = new MyClass();
     }
 }
--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,5 +1,3 @@
-package main;
-
 public class MyClass {
 
     public boolean returnTrue(){
